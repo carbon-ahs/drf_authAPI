de695889c96e17d8666ed0f3206e9460369089f2
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-01-10 16:47:40 +0600

    User can created by api

---
+++ accounts/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from accounts.models import User
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(
+        style={"input_type": "password"},
+        write_only=True,
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            "email",
+            "name",
+            "password",
+            "password2",
+        ]
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+            }
+        }
+
+    # validate password while reg
+    def validate(self, attrs):
+        password = attrs.get("password")
+        password2 = attrs.get("password2")
+        if password != password2:
+            raise serializers.ValidationError("Password does not matched.")
+        return attrs
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from core import views
+from accounts.views import UserRegistrationView
 
 
 # from .views import say_hello
 
 urlpatterns = [
-    path("", views.something_cool, name="something_cool"),
-    path("test", views.home, name="home"),
+    path("register/", UserRegistrationView.as_view(), name="register"),
 ]
